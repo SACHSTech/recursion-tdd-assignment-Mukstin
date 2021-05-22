e45d157b6f455030fbdfdb976cb58da41dd17744
Justin Ho <>
2021-05-22 02:59:17 +0000

    Comments

--- src/recursionAssignment/Recursion.java
+++ src/recursionAssignment/Recursion.java
@@ -3,17 +3,29 @@ package recursionAssignment;
 import java.io.*;
 
 public class Recursion {
+  /**
+  Prints a a random number 
+  counts the amounts of "7's" in that number
+  no loops
+  @Author Justin Ho Shue
+  */
   public static int count7(int n) {
     int counter = 0;
 
     if (n % 10 == 7)
       counter++;
-
+      
     if (n / 10 == 0)
       return counter;
 
-    return counter + count7(n / 10);
+    return counter + count7(n / 10);  
   }
+  /**
+  Prints a string
+  prints a new string 
+  but "pi" is replaced by "3.14"
+  @author Justin Ho Shue
+  */
 public static String changePi(String str) {
     String str2 = "";
     return changePi(str, str2);
@@ -33,7 +45,13 @@ public static String changePi(String str) {
     return changePi(str.substring(0, str.length() - 1), str2);
 
   }
-
+  /**
+  Prints a string
+  returns a "cleaned" string 
+  Meaning chars that are next to each other and are the same
+  will be reduced to a single char 
+  @author Justin Ho Shue
+  */
   public static String stringClean(String str) {
     if (str.length() < 2) {
       return str;
